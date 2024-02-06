631014b81c17a3eef8fafdff0a3a113922b7d901
redoxRux <adinhebbar@gmail.com>
2024-02-06 22:07:24 +0530

    first commit

---
+++ go_lower/go_lower.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func main() {
+	// Check if a file name is provided as a command-line argument
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run yourprogram.go /path/to/filename.txt")
+		os.Exit(1)
+	}
+
+	// Get the filename from the command-line argument
+	filename := os.Args[1]
+
+	// Read the content of the file
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		os.Exit(1)
+	}
+
+	// Convert uppercase to lowercase
+	modifiedContent := strings.ToLower(string(content))
+
+	// Write the modified content back to the file
+	err = ioutil.WriteFile(filename, []byte(modifiedContent), 0644)
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Uppercase to lowercase conversion complete.")
+}
